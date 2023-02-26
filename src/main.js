a551d9bc5ac33002b8d8949d89dd899cdb4bb731
Fatih SEVİM <32717966+FatihSevim@users.noreply.github.com>
2023-02-26 19:17:50 +0300

    DONE

--- src/main.js
+++ src/main.js
@@ -1,7 +1,15 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import './registerServiceWorker'
-import router from './router'
-import store from './store'
+import { createApp } from "vue";
+import App from "./App.vue";
+import "./registerServiceWorker";
+import router from "./router";
+import store from "./store";
 
-createApp(App).use(store).use(router).mount('#app')
+import "bootstrap";
+import "../node_modules/bootstrap/dist/css/bootstrap.css";
+
+import "./assets/index.scss";
+import JsBarcode from "jsbarcode";
+
+import Vue3SimpleHtml2pdf from "vue3-simple-html2pdf";
+
+createApp(App).use(store).use(router).use(Vue3SimpleHtml2pdf).mount("#app");
---
+++ src/views/Home.vue
@@ -0,0 +1,202 @@
+<template>
+	<div class="container-fluid">
+		<vue3-simple-html2pdf v-show="submitted" ref="vue3SimpleHtml2pdf" :options="pdfOptions" :filename="exportFilename">
+			<div class="container border my-3 px-5 py-3">
+				<div v-if="submitted" class="row justify-content-center">
+					<div class="col-9">
+						<div class="row mb-2">
+							<div class="col d-flex justify-content-left">
+								Country:&nbsp;&nbsp;
+								<span>
+									{{ countryCode }}
+								</span>
+							</div>
+						</div>
+						<div class="row mb-2">
+							<div class="col d-flex justify-content-left">
+								DSP:&nbsp;
+								<span>
+									{{ dspCode }}
+								</span>
+							</div>
+						</div>
+						<div class="row mb-2">
+							<div class="col d-flex justify-content-left">
+								Station:&nbsp;<span>
+									{{ stationCode }}
+								</span>
+							</div>
+						</div>
+						<div class="row mb-2">
+							<div class="col d-flex justify-content-left">
+								License Plate:&nbsp;
+								<span>
+									{{ licensePlate }}
+								</span>
+							</div>
+						</div>
+						<div class="row mb-2">
+							<div class="col d-flex justify-content-left">
+								VIN Code:&nbsp;<span>
+									{{ vinCode }}
+								</span>
+							</div>
+						</div>
+					</div>
+					<div class="col-3">
+						<QRCodeVue3
+							:value="vinCode"
+							:height="150"
+							:width="150"
+							class="qrcode"
+							fileExt="png"
+							download-button="hidden"
+							:downloadOptions="{ name: 'vqr', extension: 'png' }"
+							:qrOptions="{ typeNumber: '0', mode: 'Byte', errorCorrectionLevel: 'Q' }"
+							:imageOptions="{ hideBackgroundDots: true, imageSize: 1, margin: 0 }"
+							:dotsOptions="{ type: 'square', color: '#000000	' }"
+							:cornersSquareOptions="{ type: 'square', color: '#000000' }"
+						/>
+					</div>
+				</div>
+				<div class="row justify-content-center">
+					<div class="col">
+						<svg id="code128"></svg>
+					</div>
+				</div>
+			</div>
+		</vue3-simple-html2pdf>
+		<div v-if="submitted" class="row justify-content-center">
+			<div class="col">
+				<button @click="exportPdf()">export</button>
+			</div>
+		</div>
+		<div v-if="!submitted" class="row justify-content-center">
+			<div class="col-4 col-md-6 col-xs-12">
+				<div class="row">
+					<div class="col">
+						<span> baslik </span>
+					</div>
+				</div>
+				<div class="row">
+					<div class="col">
+						<label> Country Code: </label>
+						<input v-model="countryCode" type="text" maxlength="2" />
+					</div>
+				</div>
+				<div class="row">
+					<div class="col">
+						<label> DSP Code: </label>
+						<input v-model="dspCode" type="text" maxlength="4" />
+					</div>
+				</div>
+				<div class="row">
+					<div class="col">
+						<label> Station Code: </label>
+						<input v-model="stationCode" type="text" maxlength="4" />
+					</div>
+				</div>
+				<div class="row">
+					<div class="col">
+						<label> License Plate: </label>
+						<input v-model="licensePlate" type="text" />
+					</div>
+				</div>
+				<div class="row">
+					<div class="col">
+						<label> VIN Code: </label>
+						<input v-model="vinCode" type="text" maxlength="17" />
+					</div>
+				</div>
+				<div class="row">
+					<div class="col">
+						<button @click="handleSubmit()">Submit</button>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</template>
+
+<script>
+	import QRCodeVue3 from "qrcode-vue3";
+	export default {
+		components: {
+			QRCodeVue3,
+		},
+		data() {
+			return {
+				countryCode: "DE",
+				dspCode: "DSHP",
+				stationCode: "DBE2",
+				licensePlate: "123123",
+				vinCode: "123123",
+				submitted: true,
+
+				qrCodeUrl: "https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=",
+
+				pdfOptions: {
+					margin: 1,
+					image: {
+						type: "jpeg",
+						quality: 1,
+					},
+					html2canvas: { scale: 3 },
+					jsPDF: {
+						unit: "mm",
+						format: "a4",
+						orientation: "p",
+					},
+				},
+				exportFilename: "my-custom-file.pdf",
+			};
+		},
+		computed() {
+			//
+		},
+		methods: {
+			async handleSubmit() {
+				this.submitted = true;
+				JsBarcode("#code128", this.licensePlate);
+				this.qrCodeUrl = this.qrCodeUrl + this.vinCode;
+			},
+			exportPdf() {
+				// window.print();
+				//
+				// var qrcode = new QRCode("qr_code", {
+				// 	text: "https://cravecookie.com/",
+				// 	width: 128,
+				// 	height: 128,
+				// 	colorDark: "#000000",
+				// 	colorLight: "#ffffff",
+				// 	correctLevel: QRCode.CorrectLevel.H,
+				// });
+
+				// var pdf = new jsPDF({
+				// 	orientation: "landscape",
+				// 	unit: "mm",
+				// 	format: [84, 41],
+				// });
+
+				// pdf.setFontSize(15);
+				// pdf.text("Crave Cookie", 43, 20);
+
+				// pdf.setFontSize(10);
+				// pdf.text("Scan For Menu", 43, 25);
+
+				// let base64Image = $("#qr_code img").attr("src");
+
+				// pdf.addImage(base64Image, "png", 0, 0, 40, 40);
+
+				this.$refs.vue3SimpleHtml2pdf.download();
+			},
+		},
+	};
+</script>
+
+<style lang="scss" scoped>
+	.qrcode {
+		// height: 150px;
+		// width: 150px;
+	}
+</style>
